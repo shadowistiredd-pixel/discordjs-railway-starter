a8a7cc5948285cf4a5cf510c7f25f8a4664cc505
shadowistiredd-pixel <shadowistiredd@gmail.com>
2026-08-31 18:36:51 -0400

    Delete src directory

--- src/commands/hello.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-  data: new SlashCommandBuilder().setName("hello").setDescription("Say hello"),
-  execute: async (interaction) => {
-    await interaction.reply("Choo choo! 🚅");
-  },
-};
--- src/commands/ping.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-  data: new SlashCommandBuilder().setName("ping").setDescription("Get the bot's latency"),
-  execute: async (interaction, client) => {
-    await interaction.reply(`Pong \`${Math.round(client.ws.ping)}ms\` 🏓`);
-  },
-};
